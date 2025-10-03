c1546ca2e7c22ca347d599bcf4cc3a2108d6b9a3
arif.rahman <arif.rahman@wcma.vic.gov.au>
2025-10-03 16:01:43 +1000

    done

---
+++ src/components/MapView.tsx.js
@@ -0,0 +1,41 @@
+"use client"
+import { useEffect, useRef } from "react"
+import Map from "ol/Map"
+import View from "ol/View"
+import TileLayer from "ol/layer/Tile"
+import TileWMS from "ol/source/TileWMS"
+import LayerGroup from "ol/layer/Group"
+import "ol/ol.css"
+
+export default function MapView() {
+    const mapRef = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        if (!mapRef.current) return
+        const rasters = new TileLayer({
+            source: new TileWMS({
+                url: "http://testpozi.online/cgi-bin/qgis_mapserv.fcgi",
+                params: {
+                    LAYERS: "Stawell24RvDepthARIPMF",
+                    MAP: "/var/www/qgis_projects/flood_stawell/flood_stawell.qgs",
+                    TILED: true
+                }
+            })
+        })
+        const boundary = new TileLayer({
+            source: new TileWMS({
+                url: "http://testpozi.online/cgi-bin/qgis_mapserv.fcgi",
+                params: {
+                    LAYERS: "boundary",
+                    MAP: "/var/www/qgis_projects/flood_stawell/flood_stawell.qgs",
+                    TILED: true
+                }
+            })
+        })
+        new Map({
+            target: mapRef.current,
+            layers: [rasters, boundary],
+            view: new View({ center: [0, 0], zoom: 2 })
+        })
+    }, [])
+    return <div ref={mapRef} style={{ width: "100%", height: "100vh" }} />
+}
